7c076321d83d4171183f84e3da348766a0697593
JuanDYanez <juancho644@gmail.com>
2024-02-14 18:14:38 -0500

    Solución de bug para proceso de deploy

--- src/redux/store.d.ts
+++ src/redux/store.d.ts
@@ -1,8 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // store.d.ts
 
-declare module './redux/store' {
+declare module './redux/store.js' {
     const store: any;
+    export default store;
   }
-
-export default store;
